1de638d093699c5e1afd9cd7537a4134525834b4
2014000343-JoseOrtiz <j.ortiz.5cdb@gmail.com>
2026-04-12 20:51:03 -0600

    Ejercicio2  Guia2.cs

--- Ejercicio2  Guia2.cs
+++ Ejercicio2  Guia2.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Evaluacion de notas");
+        Console.Write("Dime tu nota en letras: ");
+        //variable
+        string nota;
+        nota = Console.ReadLine();
+        switch (nota)
+        {
+            case "A":
+                Console.WriteLine("Exelente");
+                break;
+            case "B":
+                Console.WriteLine("Bueno");
+                break ;
+            case "C":
+                Console.WriteLine("Regular");
+                break;
+            case "D":
+                Console.WriteLine("Deficiente");
+                break;
+            case "F":
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Letra invalida");
+                break;
+        }
+    }
+}
