635e30e2ca980fa67681fb5bea16a6c2ade0a713
2014000343-JoseOrtiz <j.ortiz.5cdb@gmail.com>
2026-04-12 20:48:31 -0600

    Calculadora_de_Funciones_Trig.cs

--- Calculadora_de_Funciones_Trig.cs
+++ Calculadora_de_Funciones_Trig.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("* CALCULADORA FUNCIONES TRIGONOMETRICAS BASICAS*");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("*  Lista de funciones trigonometricas     	  *");
+        Console.WriteLine("*       1- Seno                                   *");
+        Console.WriteLine("*       2- Coseno                                 *");
+        Console.WriteLine("*       3- Tangente                                *");
+        Console.WriteLine("************************************************");
+        int opc;
+        double ang, rad;
+        Console.Write("Digite el número de una función: ");
+        opc= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Dime el valor del ángulo en grados: ");
+        ang= Convert.ToDouble(Console.ReadLine());
+        rad = (ang*Math.PI)/180;
+
+        switch (opc) 
+        {
+            case 1:
+                Console.WriteLine("El Seno es: "+Math.Sin(rad));
+                break;
+            case 2:
+                Console.WriteLine("El Coseno es: " + Math.Cos(rad));
+                break ;
+            case 3:
+                Console.WriteLine("La Tangente es: "+Math.Tan(rad));
+                break;
+            default:
+                Console.WriteLine("Error al operar");
+                break;
+        }
+    }
+}
